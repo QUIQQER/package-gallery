10fd4b4de6c051afa5a83d06df7bc03fb8008ff9
Michael Danielczok <michael@pcsg.de>
2024-05-22 13:12:39 +0200

    feat: control ImageSlider as brick

--- src/QUI/Gallery/Controls/ImageSlider.php
+++ src/QUI/Gallery/Controls/ImageSlider.php
@@ -18,6 +18,8 @@ use function dirname;
  */
 class ImageSlider extends QUI\Control
 {
+    private QUI\Projects\Project $Project;
+
     /**
      * constructor
      *
@@ -31,16 +33,18 @@ class ImageSlider extends QUI\Control
             'nodeName' => 'section',
             'site' => '',
             'order' => false,
-            'limit' => false,
+            'limit' => 10,
             'moreLink' => false,
             'Project' => false,
             'folderId' => false,
+            'folderIds' => '', // ids comma separated
+            'imageBehavior' => '', // '', 'center', 'fill'
             'data-qui' => 'package/quiqqer/gallery/bin/controls/ImageSlider',
-            'height' => 200
+            'sliderHeight' => 200
         ]);
 
         $this->addCSSFile(
-            dirname(__FILE__) . '/ImageSlider.css'
+            \dirname(__FILE__) . '/ImageSlider.css'
         );
 
         parent::__construct($attributes);
@@ -55,14 +59,13 @@ class ImageSlider extends QUI\Control
     public function getBody(): string
     {
         $Engine = QUI::getTemplateManager()->getEngine();
-        $Project = $this->getProject();
-        $Media = $Project->getMedia();
-
-        /* @var $Folder Folder */
-        $Folder = $Media->get($this->getAttribute('folderId'));
-
-
+        $this->Project = $this->getProject();
         $MoreLink = null;
+        $limit = $this->getAttribute('limit');
+
+        if (!is_numeric($limit)) {
+            $limit = 10;
+        }
 
         switch ($this->getAttribute('order')) {
             case 'title DESC':
@@ -83,15 +86,36 @@ class ImageSlider extends QUI\Control
                 break;
         }
 
-        $images = $Folder->getImages([
-//            'limit' => $start . ',' . $max,
-            'order' => $order
-        ]);
+        $folderIds = $this->getAttribute('folderIds');
 
-
-        if (!$this->getAttribute('height')) {
-            $this->setAttribute('height', 200);
+        if (is_string($folderIds)) {
+            $folderIds = explode(',', $folderIds);
         }
+
+        if ($folderIds && count($folderIds) > 0) {
+            $images = $this->getImagesByFolderIds($folderIds, $order, $limit);
+        } else {
+            try {
+                /* @var $Folder \QUI\Projects\Media\Folder */
+                $Folder = QUI\Projects\Media\Utils::getMediaItemByUrl($this->getAttribute('folderId'));
+            } catch (QUI\Exception $Exception) {
+                QUI\System\Log::writeException($Exception);
+
+                return '';
+            }
+
+            $images = $Folder->getImages([
+                'limit' => $limit,
+                'order' => $order
+            ]);
+        }
+
+
+        if (!$this->getAttribute('sliderHeight')) {
+            $this->setAttribute('sliderHeight', 200);
+        }
+
+        $this->setCustomVariable('sliderHeight', $this->getAttribute('sliderHeight') . 'px');
 
         if ($this->getAttribute('moreLink')) {
             try {
@@ -100,14 +124,27 @@ class ImageSlider extends QUI\Control
             }
         }
 
+        switch ($this->getAttribute('imageBehavior')) {
+            case 'center':
+                $imageBehavior = 'quiqqer-gallery-imageSlider--image-center';
+                break;
+
+            case 'fill':
+                $imageBehavior = 'quiqqer-gallery-imageSlider--image-fill';
+                break;
+
+            default:
+                $imageBehavior = '';
+        }
+
         $Engine->assign([
             'this' => $this,
             'images' => $images,
-            'MoreLink' => $MoreLink
+            'MoreLink' => $MoreLink,
+            'imageBehavior' => $imageBehavior
         ]);
 
-
-        return $Engine->fetch(dirname(__FILE__) . '/ImageSlider.html');
+        return $Engine->fetch($this->getTemplate());
     }
 
     /**
@@ -117,6 +154,90 @@ class ImageSlider extends QUI\Control
      */
     protected function getTemplate(): string
     {
-        return dirname(__FILE__) . '/ImageSlider.html';
+        return \dirname(__FILE__) . '/ImageSlider.html';
+    }
+
+    /**
+     * Set custom css variable to the control as inline style
+     * --_qui-gridAdvanced-$name: var(--qui-gridAdvanced-$name, $value);
+     *
+     * @param $name
+     * @param $value
+     * @return void
+     */
+    private function setCustomVariable($name, $value): void
+    {
+        if (!$name || !$value) {
+            return;
+        }
+
+        $this->setStyle(
+            '--_qui-gallery-imageSlider-' . $name,
+            'var(--qui-gallery-imageSlider-' . $name . ', ' . $value . ')'
+        );
+    }
+
+    /**
+     * Get images from multiple folders (direct SQL query).
+     *
+     * @param array $folderIds
+     * @param string $order
+     * @param int $limit
+     * @return array
+     */
+    private function getImagesByFolderIds(array $folderIds, string $order, int $limit): array
+    {
+        $table = QUI::getDBTableName($this->Project->getAttribute('name') . '_media');
+        $table_rel = QUI::getDBTableName($this->Project->getAttribute('name') . '_media_relations');
+
+        $whereClause = [
+            $table_rel . '.child = ' . $table . '.id',
+            $table . '.deleted = 0 ',
+            $table . '.type = \'image\''
+        ];
+
+        $folderConditions = [];
+
+        foreach ($folderIds as $folderId) {
+            $folderConditions[] = $table_rel . '.parent = ' . (int)$folderId;
+        }
+
+        $whereClause[] = '(' . implode(' OR ', $folderConditions) . ')';
+        $whereClauseString = implode(' AND ', $whereClause);
+
+        $dbQuery = [
+            'select' => 'id',
+            'from' => [
+                $table,
+                $table_rel
+            ],
+            'limit' => $limit,
+            'active' => 1,
+            'where' => $whereClauseString
+        ];
+
+        $dbQuery['order'] = $order;
+
+        // database
+        try {
+            $fetch = QUI::getDataBase()->fetch($dbQuery);
+        } catch (QUI\Exception $Exception) {
+            QUI\System\Log::writeException($Exception);
+
+            return [];
+        }
+
+        $result = [];
+
+        foreach ($fetch as $entry) {
+            try {
+                $Media = $this->Project->getMedia();
+                $result[] = $Media->get((int)$entry['id']);
+            } catch (QUI\Exception $Exception) {
+                QUI\System\Log::addDebug($Exception->getMessage());
+            }
+        }
+
+        return $result;
     }
 }
