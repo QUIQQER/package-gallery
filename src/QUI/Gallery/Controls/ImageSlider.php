292f4721b2926debef7b67bd93bc79f0a92f8dea
Michael Danielczok <michael@pcsg.de>
2024-05-28 11:14:52 +0200

    fix(ImageSlider): consider random order when get images by folder

--- src/QUI/Gallery/Controls/ImageSlider.php
+++ src/QUI/Gallery/Controls/ImageSlider.php
@@ -128,6 +128,11 @@ class ImageSlider extends QUI\Control
             }
 
             $images = $Folder->getImages($query);
+
+            if ($shuffleImages && $limit) {
+                shuffle($images);
+                $images = array_slice($images, 0, $limit);
+            }
         }
 
         if (!$this->getAttribute('sliderHeight')) {
